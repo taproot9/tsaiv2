f954acb2679240f2c738d3e2f028940f9c32b9e0
Marimar Dela Cerna <marimardelacerna@Marimars-MacBook-Pro.local>
2017-06-04 19:33:58 +0800

    working in UI offers

--- resources/views/offers.blade.php
+++ resources/views/offers.blade.php
@@ -17,205 +17,35 @@
         <div class="line">
             <!--            <a class="button rounded-full-btn margin-bottom right" href="#"><span class="icon-sli-plus pull-right"></span></a>-->
             <a class="button rounded-full-btn margin-bottom right" href="#">
-                <img  src="{{asset('tsai_bootstrap/img/ic_add_white_24dp_1x.png')}}">
+                <img src="{{asset('tsai_bootstrap/img/ic_add_white_24dp_1x.png')}}">
             </a>
         </div>
 
+
         <div class="line">
-            <div class="margin">
-                <div class="s-12 m-6 l-4">
-                    <h2 style="text-align: left">Title</h2>
-                    <div class="myContainer">
-                        <img class="image" src="{{asset('tsai_bootstrap/img/second.jpg')}}" alt="Avatar" class="image">
-                        <div class="middle">
-                            <a class="button rounded-full-btn submit-btn margin-bottom" href="#">
-                                <img src="{{asset('tsai_bootstrap/img/ic_mode_edit_white_24dp_1x.png')}}">
-                            </a>
-                            <a class="button rounded-full-btn cancel-btn margin-bottom" href="#">
-                                <img src="{{asset('tsai_bootstrap/img/ic_delete_forever_white_24dp_1x.png')}}">
+            <div class="margin" id="offers_list">
+
+
+                <div class="l-12">
+                    <div class="s-12 m-12 l-12">
+                        <div class="s-12 m-8 l-4">
+                            <h2 style="text-align: left" class="s-12 m-8 l-4">Titles</h2>
+                                <img class="image" src="{{asset('tsai_bootstrap/img/second.jpg')}}" alt="Avatar"
+                                     class="image">
+                        </div>
+                        <div class="s-12 m-8 l-8">
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                            <a href="">
+                                <span onclick="hide(); return false">Details</span>
                             </a>
                         </div>
                     </div>
-
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                    <a href="">
-                        <span onclick="hide(); return false">Details</span>
-                    </a>                </p>
                 </div>
-                <div class="s-12 m-6 l-4">
-                    <h2 style="text-align: left">Title</h2>
-                    <div class="myContainer">
-                        <img class="image" src="{{asset('tsai_bootstrap/img/second.jpg')}}" alt="Avatar" class="image">
-                        <div class="middle">
-                            <a class="button rounded-full-btn submit-btn margin-bottom" href="#">
-                                <img src="{{asset('tsai_bootstrap/img/ic_mode_edit_white_24dp_1x.png')}}">
-                            </a>
-                            <a class="button rounded-full-btn cancel-btn margin-bottom" href="#">
-                                <img src="{{asset('tsai_bootstrap/img/ic_delete_forever_white_24dp_1x.png')}}">
-                            </a>
-                        </div>
-                    </div>
 
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                    <a href="">
-                        <span onclick="hide(); return false">Details</span>
-                    </a>                </p>
-                </div>
-                <div class="s-12 m-6 l-4">
-                    <h2 style="text-align: left">Title</h2>
-                    <div class="myContainer">
-                        <img class="image" src="{{asset('tsai_bootstrap/img/second.jpg')}}" alt="Avatar" class="image">
-                        <div class="middle">
-                            <a class="button rounded-full-btn submit-btn margin-bottom" href="#">
-                                <img src="{{asset('tsai_bootstrap/img/ic_mode_edit_white_24dp_1x.png')}}">
-                            </a>
-                            <a class="button rounded-full-btn cancel-btn margin-bottom" href="#">
-                                <img src="{{asset('tsai_bootstrap/img/ic_delete_forever_white_24dp_1x.png')}}">
-                            </a>
-                        </div>
-                    </div>
-
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                    <a href="">
-                        <span onclick="hide(); return false">Details</span>
-                    </a>                </p>
-                </div>
-                <div class="s-12 m-6 l-4">
-                    <h2 style="text-align: left">Title</h2>
-                    <div class="myContainer">
-                        <img class="image" src="{{asset('tsai_bootstrap/img/second.jpg')}}" alt="Avatar" class="image">
-                        <div class="middle">
-                            <a class="button rounded-full-btn submit-btn margin-bottom" href="#">
-                                <img src="{{asset('tsai_bootstrap/img/ic_mode_edit_white_24dp_1x.png')}}">
-                            </a>
-                            <a class="button rounded-full-btn cancel-btn margin-bottom" href="#">
-                                <img src="{{asset('tsai_bootstrap/img/ic_delete_forever_white_24dp_1x.png')}}">
-                            </a>
-                        </div>
-                    </div>
-
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                    <a href="">
-                        <span onclick="hide(); return false">Details</span>
-                    </a>                </p>
-                </div>
-                <div class="s-12 m-6 l-4">
-                    <h2 style="text-align: left">Title</h2>
-                    <div class="myContainer">
-                        <img class="image" src="{{asset('tsai_bootstrap/img/second.jpg')}}" alt="Avatar" class="image">
-                        <div class="middle">
-                            <a class="button rounded-full-btn submit-btn margin-bottom" href="#">
-                                <img src="{{asset('tsai_bootstrap/img/ic_mode_edit_white_24dp_1x.png')}}">
-                            </a>
-                            <a class="button rounded-full-btn cancel-btn margin-bottom" href="#">
-                                <img src="{{asset('tsai_bootstrap/img/ic_delete_forever_white_24dp_1x.png')}}">
-                            </a>
-                        </div>
-                    </div>
-
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                    <a href="">
-                        <span onclick="hide(); return false">Details</span>
-                    </a>                </p>
-                </div>
-                <div class="s-12 m-6 l-4">
-                    <h2 style="text-align: left">Title</h2>
-                    <div class="myContainer">
-                        <img class="image" src="{{asset('tsai_bootstrap/img/second.jpg')}}" alt="Avatar" class="image">
-                        <div class="middle">
-                            <a class="button rounded-full-btn submit-btn margin-bottom" href="#">
-                                <img src="{{asset('tsai_bootstrap/img/ic_mode_edit_white_24dp_1x.png')}}">
-                            </a>
-                            <a class="button rounded-full-btn cancel-btn margin-bottom" href="#">
-                                <img src="{{asset('tsai_bootstrap/img/ic_delete_forever_white_24dp_1x.png')}}">
-                            </a>
-                        </div>
-                    </div>
-
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                    <a href="">
-                        <span onclick="hide(); return false">Details</span>
-                    </a>                </p>
-                </div>
-                <!--            <div class="s-12 m-6 l-4">-->
-                <!--                <button type="button" style="border-color: transparent; background-color: transparent">-->
-                <!--                    <img src="{{asset('tsai_bootstrap/img/second.jpg')}}">-->
-                <!--                </button>-->
-                <!--                <p class="subtitile">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.-->
-                <!--                </p>-->
-                <!--            </div>-->
-                <!--            <div class="s-12 m-6 l-4">-->
-                <!--                <img src="{{asset('tsai_bootstrap/img/second.jpg')}}">-->
-                <!--                <p class="subtitile">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.-->
-                <!--                </p>-->
-                <!--            </div>-->
-                <!--            <div class="s-12 m-6 l-4">-->
-                <!--                <img src="{{asset('tsai_bootstrap/img/second.jpg')}}">-->
-                <!--                <p class="subtitile">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.-->
-                <!--                </p>-->
-                <!--            </div>-->
-                <!--            <div class="s-12 m-6 l-4">-->
-                <!--                <img src="{{asset('tsai_bootstrap/img/second.jpg')}}">-->
-                <!--                <p class="subtitile">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.-->
-                <!--                </p>-->
-                <!--            </div>-->
-                <!--            <div class="s-12 m-6 l-4">-->
-                <!--                <img src="{{asset('tsai_bootstrap/img/second.jpg')}}">-->
-                <!--                <p class="subtitile">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.-->
-                <!--                </p>-->
-                <!--            </div>-->
-                <!--            <div class="s-12 m-6 l-4">-->
-                <!--                <img src="{{asset('tsai_bootstrap/img/second.jpg')}}">-->
-                <!--                <p class="subtitile">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.-->
-                <!--                </p>-->
-                <!--            </div>-->
-                <!--            <div class="s-12 m-6 l-4">-->
-                <!--                <img src="{{asset('tsai_bootstrap/img/second.jpg')}}">-->
-                <!--                <p class="subtitile">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.-->
-                <!--                </p>-->
-                <!--            </div>-->
-                <!--            <div class="s-12 m-6 l-4">-->
-                <!--                <img src="{{asset('tsai_bootstrap/img/second.jpg')}}">-->
-                <!--                <p class="subtitile">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.-->
-                <!--                </p>-->
-                <!--            </div>-->
-                <!--            <div class="s-12 m-6 l-4">-->
-                <!--                <img src="{{asset('tsai_bootstrap/img/second.jpg')}}">-->
-                <!--                <p class="subtitile">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.-->
-                <!--                </p>-->
-                <!--            </div>-->
-                <!--            <div class="s-12 m-6 l-4">-->
-                <!--                <img src="{{asset('tsai_bootstrap/img/second.jpg')}}">-->
-                <!--                <p class="subtitile">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.-->
-                <!--                </p>-->
-                <!--            </div>-->
-                <!--            <div class="s-12 m-6 l-4">-->
-                <!--                <img src="{{asset('tsai_bootstrap/img/second.jpg')}}">-->
-                <!--                <p class="subtitile">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.-->
-                <!--                </p>-->
-                <!--            </div>-->
-                <!--            <div class="s-12 m-6 l-4">-->
-                <!--                <img src="{{asset('tsai_bootstrap/img/second.jpg')}}">-->
-                <!--                <p class="subtitile">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.-->
-                <!--                </p>-->
-                <!--            </div>-->
             </div>
-
-
         </div>
-
     </div>
 
 
@@ -224,25 +54,37 @@
             <div id="owl-demo2" class="owl-carousel owl-theme">
                 <div class="item">
                     <h2>Amazing responsive template</h2>
-                    <p class="s-12 m-12 l-8 center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-                        nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim
-                        veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+                    <p class="s-12 m-12 l-8 center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+                        diam
+                        nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim
+                        ad
+                        minim
+                        veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea
+                        commodo
                         consequat.
                     </p>
                 </div>
                 <div class="item">
                     <h2>Responsive components</h2>
-                    <p class="s-12 m-12 l-8 center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-                        nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim
-                        veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+                    <p class="s-12 m-12 l-8 center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+                        diam
+                        nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim
+                        ad
+                        minim
+                        veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea
+                        commodo
                         consequat.
                     </p>
                 </div>
                 <div class="item">
                     <h2>Retina ready</h2>
-                    <p class="s-12 m-12 l-8 center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-                        nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim
-                        veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+                    <p class="s-12 m-12 l-8 center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+                        diam
+                        nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim
+                        ad
+                        minim
+                        veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea
+                        commodo
                         consequat.
                     </p>
                 </div>
@@ -250,4 +92,6 @@
         </div>
     </div>
 </section>
+<!--<script src="{{asset("tsai_bootstrap/js/offers.js")}}"></script>-->
+
 @endsection
